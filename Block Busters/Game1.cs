4c278f77f9c967103a15860758eaaa99766a6310
magicjedi90 <magicjedi90@aim.com>
2013-11-08 13:29:54 -0800

    Changed background color

--- Block Busters/Game1.cs
+++ Block Busters/Game1.cs
@@ -81,7 +81,7 @@ namespace Block_Busters
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Gray);
 
             // TODO: Add your drawing code here
 
